a06f0fd546e6bf57c620d2371ee2f01098cf8597
xeounxzxu <pnw123456@icloud.com>
2025-10-03 13:08:57 +0900

    setting project

---
+++ src/main/java/io/xeounxzxu/springbatchextensions/SpringBatchExtensionsApplication.java
@@ -0,0 +1,13 @@
+package io.xeounxzxu.springbatchextensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExtensionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchExtensionsApplication.class, args);
+	}
+
+}
