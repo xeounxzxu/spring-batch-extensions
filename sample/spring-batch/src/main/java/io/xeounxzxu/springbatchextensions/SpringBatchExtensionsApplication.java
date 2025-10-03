34b3bc1993ce7208f3e29f44e2821ffc07d8f582
xeounxzxu <pnw123456@icloud.com>
2025-10-03 13:24:09 +0900

    build: project setting

--- sample/spring-batch/src/main/java/io/xeounxzxu/springbatchextensions/SpringBatchExtensionsApplication.java
+++ sample/spring-batch/src/main/java/io/xeounxzxu/springbatchextensions/SpringBatchExtensionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBatchExtensionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchExtensionsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExtensionsApplication.class, args);
+    }
 }
